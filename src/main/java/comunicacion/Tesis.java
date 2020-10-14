ecc98457612ad4a0e3bdd3e3952d35da6fe0daab
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-10-13 21:20:02 -0500

    ass class

---
+++ src/main/java/comunicacion/Tesis.java
@@ -0,0 +1,77 @@
+package comunicacion;
+
+public class Tesis extends Escrito {
+	private String idea;
+	private String[] argumentos;
+	private String conclucion;
+	private String referencias;
+	private String interpretacion;
+	
+	
+	
+	
+	
+	public Tesis(String origen, String titulo, String autor, int paginas) {
+		super(origen, titulo, autor, paginas);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	int palabrasTotales(int palabrasPagina) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	String interpretacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getIdea() {
+		return idea;
+	}
+
+	public void setIdea(String idea) {
+		this.idea = idea;
+	}
+
+	public String[] getArgumentos() {
+		return argumentos;
+	}
+
+	public void setArgumentos(String[] argumentos) {
+		this.argumentos = argumentos;
+	}
+
+	public String getConclucion() {
+		return conclucion;
+	}
+
+	public void setConclucion(String conclucion) {
+		this.conclucion = conclucion;
+	}
+
+	public String getReferencias() {
+		return referencias;
+	}
+
+	public void setReferencias(String referencias) {
+		this.referencias = referencias;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+
+}
