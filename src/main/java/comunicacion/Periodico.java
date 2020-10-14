3a09f09fc92f495f8350f80895107330f100c086
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-10-13 22:11:23 -0500

    periodico todo

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -9,11 +9,16 @@ public class Periodico extends Escrito{
 		super(origen, titulo, autor, paginas);
 		// TODO Auto-generated constructor stub
 	}
-
+	public Periodico(String origen, String titulo, String autor, int paginas, String f, String p, String i) {
+		super(origen, titulo, autor, paginas);
+		fecha=f;
+		primicia=p;
+		interpretacion=i;
+	}
 	@Override
 	int palabrasTotales(int palabrasPagina) {
 		// TODO Auto-generated method stub
-		return 0;
+		return (getPaginas()*palabrasPagina*10);
 	}
 
 	@Override
@@ -25,7 +30,7 @@ public class Periodico extends Escrito{
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		return (getOrigen() + "\n" + getTitulo() + "\n" + getAutor()+ "\n"  + getPaginas() + "\n" + fecha +"\n"+ primicia);
 	}
 
 	public String getFecha() {
